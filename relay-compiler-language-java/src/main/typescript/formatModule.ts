174281b4cffc99a3f33c9c3e675e8e93d933fbda
Edvin Eriksson <edvinerikson@gmail.com>
2020-02-09 16:57:15 +0100

    configure plugin

--- relay-compiler-language-java/src/main/typescript/formatModule.ts
+++ relay-compiler-language-java/src/main/typescript/formatModule.ts
@@ -1,7 +1,26 @@
 import { FormatModule } from "relay-compiler";
+import { print } from "./codegen";
+import { format } from "prettier";
 
-const formatModule: FormatModule = ({ definition, node }) => {
-  return "";
-};
+export function formatModuleFactory({
+  packageName
+}: {
+  packageName: string;
+}): FormatModule {
+  const formatModule: FormatModule = ({ docText, node }) => {
+    const module = `package ${packageName};
+  import dev.wennerdahl.relay.compiler.language.java.runtime.*;
+  /**
+  ${docText}
+  */
 
-export default formatModule;
+  ${print(node)}
+  `;
+
+    return format(module, {
+      filepath: "source.java",
+      plugins: [require("prettier-plugin-java")]
+    });
+  };
+  return formatModule;
+}
--- relay-compiler-language-java/src/main/typescript/main.ts
+++ relay-compiler-language-java/src/main/typescript/main.ts
@@ -4,19 +4,32 @@ import {
   GraphQLTagFinder,
   TypeGenerator
 } from "relay-compiler/lib/language/RelayLanguagePluginInterface";
-import formatModule from "./formatModule";
+import { formatModuleFactory } from "./formatModule";
+import { loadConfig } from "relay-config";
+import invariant from "invariant";
 
 const noopFindGraphQLTags: GraphQLTagFinder = (): GraphQLTag[] => [];
 const noopTypeGenerator: TypeGenerator = {
   generate: (schema, node) => "",
   transforms: []
 };
+
+export { formatModuleFactory };
+
 export default function plugin(): PluginInterface {
+  const config = loadConfig();
+  invariant(
+    config?.artifactDirectory != null,
+    "Must set artifactDirectory to use relay-compiler-language-java."
+  );
+  const artifactDirectory = config.artifactDirectory;
+  const packageName = artifactDirectory.split("/").join(".");
+
   return {
     inputExtensions: ["java"],
     outputExtension: "java",
     findGraphQLTags: noopFindGraphQLTags,
-    formatModule: formatModule,
+    formatModule: formatModuleFactory({ packageName }),
     typeGenerator: noopTypeGenerator
   };
 }
